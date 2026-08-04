19d7ac93bf4dba66a3682f61649ff333a3304d95
TanviPatel18 <pateltanvi2528@gmail.com>
2026-08-04 11:41:20 +0530

    add some basic syntex and menthods

---
+++ basic.py
@@ -0,0 +1,75 @@
+name=" boo"
+age=29
+
+#  this is the  comment formet
+
+print(type(name))
+
+print(type(name)== str)
+
+print(isinstance(name,int)) #check variable type and givve the type
+
+#  oprater
+
+1+1 #2
+2-1 #1
+2*2 #4
+4/2 #2
+4%3 #1
+4**2 #16
+print(5//2) #2
+
+
+print("scam"+ " is not done by any one")
+
+a=1
+b=2
+a==b
+a!=b
+a>b
+a<=b
+
+
+# boolen oprater:- and or not
+print(6 or 7)
+print('noii' or "booo")
+print('hy ' or 'hiii')
+print('plzz' and 'fixxx')
+print( not 'yes')
+
+# ternary operator
+#  syntex
+# vvalue_if_true if condition else value
+
+print("adult"if age>18 else"minor")
+
+# string
+print("""booooo is
+
+    28year
+
+    old
+
+    yes!!!!
+    """)
+
+#  string methods
+
+print("mayra".upper())
+print("mayra".lower())
+print("mayra solutions".title())
+print("mayra".islower())
+print("mayra".isupper())
+
+# user input
+# number=input("enter the number:-")
+# print("you are select :- " +number)
+
+# control statment
+condition =""
+Name="booo"
+if condition==True:
+    print("\nthe condition")
+    print("was true")
+elif Name=="booo":
+    print("welcome,"+Name+"!!!!")
