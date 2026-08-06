890f87fc79e576c53a9e9b3c467acc40fc921403
TanviPatel18 <pateltanvi2528@gmail.com>
2026-08-06 12:53:37 +0530

    some syntex and method about list

---
+++ lists.py
@@ -0,0 +1,26 @@
+dogs=["roger" ,"wow","true"]
+print(dogs)
+items=dogs.sort()# not variable print it return none but u print a list it give the sorted list
+
+# dogs.sort()
+print(dogs)
+
+dogs[0]="booo"
+print(dogs)
+
+
+# add new in the list
+dogs.append("judah")
+# dogs.extend("nnew")
+dogs.extend(["nnew"])
+dogs+=["bod","nooo"]
+print(dogs)
+
+dogs.insert(2,"test")
+print(dogs)
+#  delete the aliment
+dogs.remove("nnew")
+print(dogs) 
+
+print(dogs.pop())
+
